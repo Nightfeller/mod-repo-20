0b137e553b9d06846caa3be6c66b4e771ac9e7e1
Nightfeller <family2earls@gmail.com>
2024-01-19 22:51:06 -0600

    added footer. next up is projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import HeaderJSX from './components/Header'
+import FooterJSX from './components/Footer'
 
 function App() {
   return (
     <>
       <HeaderJSX />
+      <FooterJSX />
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 function FooterJSX() {
     return (
         <>
-        
+            <footer className="flex bg-paletteRichBlack justify-center align-end">
+                <a>Hello.</a>
+            </footer>
         </>
     );
 }
 
-export default FooterJSX();
+export default FooterJSX;
