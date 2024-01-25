c05e4460be57136bd4f6c962be35524c272f7a45
Nightfeller <family2earls@gmail.com>
2024-01-24 19:07:27 -0600

    added imports and info for Project.jsx, but it doesn't work yet

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,11 +1,26 @@
-
+import vibeify from '../assets/vibeify.png';
+import dndBuilder from '../assets/dnd_builder.png';
+import placeholderImage from '../assets/placeholder-image.png';
 
 export default function ProjectJSX() {
     return (
         <>
             <div className="flex justify-center">
+                <p className="pt-6">Vibeify</p>
                 <p>
-                <a href="https://nataniel-c.github.io/vibeify/"><img src="" alt="Project 1: Vibify" /></a>
+                <a href="https://nataniel-c.github.io/vibeify/"><img src={vibeify} alt="Project 1: Vibify" /></a>
+                </p>
+            </div>
+            <div className="flex justify-center">
+                <p className="pt-6">DnD Builder</p>
+                <p>
+                <a href="https://dnd-builder-8e53886511db.herokuapp.com/"><img src={dndBuilder} alt="Project 2: DnD Builder" /></a>
+                </p>
+            </div>
+            <div className="flex justify-center">
+                <p className="pt-6">NaN</p>
+                <p>
+                <a href="#"><img src={placeholderImage} alt="Project 3: LFG" /></a>
                 </p>
             </div>
         </>
