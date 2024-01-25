c410a08d9cd9613f23beb942790e23235b0acded
Nightfeller <family2earls@gmail.com>
2024-01-24 22:45:12 -0600

    tried to fix image errors, still not working

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,6 @@
-import vibeify from '../assets/vibeify.png';
-import dndBuilder from '../assets/dnd_builder.png';
-import placeholderImage from '../assets/placeholder-image.png';
+// import vibeify from '../assets/vibeify.png';
+// import dndBuilder from '../assets/dnd_builder.png';
+// import placeholderImage from '../assets/placeholder-image.webp';
 
 export default function ProjectJSX() {
     return (
@@ -8,19 +8,19 @@ export default function ProjectJSX() {
             <div className="flex justify-center">
                 <p className="pt-6">Vibeify</p>
                 <p>
-                <a href="https://nataniel-c.github.io/vibeify/"><img src={vibeify} alt="Project 1: Vibify" /></a>
+                <a href="https://nataniel-c.github.io/vibeify/"><img src="" alt="Project 1: Vibify" /></a>
                 </p>
             </div>
             <div className="flex justify-center">
                 <p className="pt-6">DnD Builder</p>
                 <p>
-                <a href="https://dnd-builder-8e53886511db.herokuapp.com/"><img src={dndBuilder} alt="Project 2: DnD Builder" /></a>
+                <a href="https://dnd-builder-8e53886511db.herokuapp.com/"><img src="" alt="Project 2: DnD Builder" /></a>
                 </p>
             </div>
             <div className="flex justify-center">
                 <p className="pt-6">NaN</p>
                 <p>
-                <a href="#"><img src={placeholderImage} alt="Project 3: LFG" /></a>
+                <a href="#"><img src="" alt="Project 3: LFG" /></a>
                 </p>
             </div>
         </>
